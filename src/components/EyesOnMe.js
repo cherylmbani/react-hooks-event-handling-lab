a3645aa837a285c38fc66293cc9593d4408a051b
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-07-10 11:10:39 +0300

    adding events to components

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    function focusOnMe(){
+        console.log('Good!');
+    }
+    function blurMe(){
+        console.log('Hey! Eyes on me!')
+    }
+
+    return(
+        <div>
+            <button onFocus={focusOnMe} onBlur={blurMe}>Eyes on me</button>
+        </div>
+    )
+}
+export default EyesOnMe;
